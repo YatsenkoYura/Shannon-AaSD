d6e25dc82712b9bf1417b889811e11289de9ebc5
YatsenkoYura <st135624@student.spbu.ru>
2025-05-13 23:54:24 +0300

    add useless argc use in main for fix warning

--- main.cpp
+++ main.cpp
@@ -4,6 +4,12 @@
 #include "decode/decode.hpp"
 
 int main(int argc, char* argv[]) {
+
+    if (argc < 5) {
+        std::cerr << "Usage: <program> <-e/-d> <input> <output> <key>\n";
+        return 1;
+    }
+    
     std::string mode = argv[1];
     try {
         if (mode == "-e") {
